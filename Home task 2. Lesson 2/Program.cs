23473f71ce73fa39de32ac954385db8110b1aec0
Khametov Bakir <bakir_90@inbox.ru>
2022-10-07 20:26:54 +0300

    add tak 2

---
+++ Home task 2. Lesson 2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 13: Напишите программу, 
+которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.Clear();
+Console.WriteLine("Введите  число");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99)
+{
+    int number3 = number/10 %10;
+    Console.WriteLine($"Число {number3}  является третьей цифрой");return;
+}
+if (number < -99)
+{
+    int number3 = (number/10 %10)* -1;
+    Console.WriteLine($"Число {number3}  является  третьей цифрой");return;
+}
+else
+{
+    Console.WriteLine($"Число {number}  не имеет  третью цифру. Введите число соответствующее условиям задачи");
+}
